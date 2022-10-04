5bec38c9a2422c45acb7ec6638d33bfb46dc7988
Matthewss <112508000+Christopher-MCT@users.noreply.github.com>
2022-10-04 11:35:44 -0500

    primer-upload

---
+++ red-social/src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="Nav-bar">
+        barra de nav
+      </div>
+
+      <div className="seccion-1">
+      <button name="button">Click 1</button>
+      <button name="button">Click 2</button>
+      <button name="button">Click 3</button>
+      <button name="button">Click 5</button>
+      <button name="button">Click 6</button>
+      </div>
+
+      <div className="seccion-2">
+        <div className="Publicaciones">
+          hellow
+        </div>
+      
+
+      </div>
+
+      <div className="seccion-3">
+      <button name="button">Click 1</button>
+      <button name="button">Click 2</button>
+      <button name="button">Click 3</button>
+      <button name="button">Click 5</button>
+      <button name="button">Click 6</button> 
+      
+      </div>
+     
+
+    </div>
+  );
+}
+
+export default App;
