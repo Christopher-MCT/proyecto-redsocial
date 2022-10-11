dda2aa9dd22d4ffa9f1907df04f97916ddd4320f
Matthewss <112508000+Christopher-MCT@users.noreply.github.com>
2022-10-11 14:39:28 -0500

    cambio-css

--- red-social/src/App.js
+++ red-social/src/App.js
@@ -6,33 +6,32 @@ function App() {
   return (
     <div className="App">
 
-      <div className="Nav-bar">
-        barra de nav
+      <div className="nav-bar">
+        <form className="search">
+	        <input type="text" placeholder="Buscar..." className="input"></input>
+	      </form>
       </div>
 
       <div className="seccion-1">
-      <button name="button">Click 1</button>
-      <button name="button">Click 2</button>
-      <button name="button">Click 3</button>
-      <button name="button">Click 5</button>
-      <button name="button">Click 6</button>
+          <button name="button">Click 1</button>
+          <button name="button">Click 2</button>
+          <button name="button">Click 3</button>
+          <button name="button">Click 5</button>
+          <button name="button">Click 6</button>
       </div>
 
       <div className="seccion-2">
         <div className="Publicaciones">
           hellow
         </div>
-      
-
       </div>
 
       <div className="seccion-3">
-      <button name="button">Click 1</button>
-      <button name="button">Click 2</button>
-      <button name="button">Click 3</button>
-      <button name="button">Click 5</button>
-      <button name="button">Click 6</button> 
-      
+          <button name="button">Click 1</button>
+          <button name="button">Click 2</button>
+          <button name="button">Click 3</button>
+          <button name="button">Click 5</button>
+          <button name="button">Click 6</button> 
       </div>
      
 
